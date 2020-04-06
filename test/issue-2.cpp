1a43db3f06f651e7501d29088e26300ad4f0087b
Andrew Bell <andrew.bell.ia@gmail.com>
2020-04-06 10:00:51 -0400

    Update tests.

--- test/issue-2.cpp
+++ test/issue-2.cpp
@@ -20,8 +20,13 @@ struct Point2D {
     {}
 };
 
-Point2D rotate(Point2D const& p_in, Point2D const& center,
-    double const& angle_deg)
+std::ostream& operator<<(std::ostream& out, const Point2D& p)
+{
+    out << p.x << "/" << p.y;
+    return out;
+}
+
+Point2D rotate(Point2D const& p_in, double const& angle_deg)
 {
     const double pi        = 3.14159265358979323846;
     const double angle_rad = angle_deg * pi / 180.0;
@@ -30,17 +35,12 @@ Point2D rotate(Point2D const& p_in, Point2D const& center,
     Point2D p;
 
     // translate point back to origin:
-    p.x = p_in.x - center.x;
-    p.y = p_in.y - center.y;
+    p = p_in;
 
     // rotate point
     double tmp = p.x * c - p.y * s;
     p.y = p.x * s + p.y * c;
     p.x = tmp;
-
-    // translate point back:
-    p.x += center.x;
-    p.y += center.y;
 
     return p;
 }
@@ -57,18 +57,15 @@ bool approx(double v1, double v2)
     return fabs(v1 - v2) < eps;
 }
 
-TEST(External_Lib_Delaunator_Test, rotate_grid_6_8_8_207_0)
+void testRotation(double angle)
 {
-    const size_t  grid_size = 6;
-    const Point2D center(8, 8);
-    const double  angle = 207.0;
-
+    const size_t grid_size = 6;
     std::vector<double> points;
 
     // Generate rotated point grid of 1-by-1 squares.
     for (size_t x = 0; x < grid_size; ++x) {
         for (size_t y = 0; y < grid_size; ++y) {
-            Point2D p = rotate(Point2D(x, y), center, angle);
+            Point2D p = rotate(Point2D(x, y), angle);
             points.push_back(p.x);
             points.push_back(p.y);
         }
@@ -88,6 +85,9 @@ TEST(External_Lib_Delaunator_Test, rotate_grid_6_8_8_207_0)
 
     for (size_t i = 0; i < delaunator.triangles.size(); i += 3)
     {
+        size_t i0 = delaunator.triangles[i];
+        size_t i1 = delaunator.triangles[i + 1];
+        size_t i2 = delaunator.triangles[i + 2];
         Point2D p1(points[2 * delaunator.triangles[i]],
                    points[1 + 2 * delaunator.triangles[i]]);
         Point2D p2(points[2 * delaunator.triangles[i + 1]],
@@ -104,37 +104,36 @@ TEST(External_Lib_Delaunator_Test, rotate_grid_6_8_8_207_0)
                              p3.x * (p1.y - p2.y)) / 2.0);
 
         // Check edge lengths according to (1).
-        bool cond_1 = false;
         if (approx(len_1, sqrt(2.0)))
         {
-            cond_1 = approx(len_2, 1.0) && approx(len_3, 1.0);
+            EXPECT_TRUE(approx(len_2, 1.0) && approx(len_3, 1.0)) <<
+                "Bad triangle angle/lengths = " << angle << "/" <<
+                len_1 << " " << len_2 << " " << len_3;
         }
         else if (approx(len_2, sqrt(2.0)))
         {
-            cond_1 = approx(len_1, 1.0) && approx(len_3, 1.0);
+            EXPECT_TRUE(approx(len_1, 1.0) && approx(len_3, 1.0)) <<
+                "Bad triangle angle/lengths = " << angle << "/" <<
+                len_1 << " " << len_2 << " " << len_3;
         }
         else if (approx(len_3, sqrt(2.0)))
         {
-            cond_1 = approx(len_1, 1.0) && approx(len_2, 1.0);
+            EXPECT_TRUE(approx(len_1, 1.0) && approx(len_2, 1.0)) <<
+                "Bad triangle angle/lengths = " << angle << "/" <<
+                len_1 << " " << len_2 << " " << len_3;
         }
-
-        // Check triangle area according to (2).
-        bool cond_2 = false;
-        if (cond_1)
+        else
         {
-            // Triangle seems regular half 1-b-1 square. Just to be sure,
-            // check the area.
-            cond_2 = approx(area, 0.5);
+            ADD_FAILURE() <<
+                "Bad triangle angle/lengths = " << angle << "/" <<
+                len_1 << " " << len_2 << " " << len_3;
         }
-        else {
-            // Irregular triangle. Accept degenerate ones. (Assume they
-            // are on the outer egde, but do not check.)
-            cond_2 = area < 0.01;
-        }
-
-        EXPECT_TRUE(cond_2) << "triangle = "
-            "[" << p1.x << ", " << p1.y << "]; "
-            "[" << p2.x << ", " << p2.y << "]; "
-            "[" << p3.x << ", " << p3.y << "]";
     }
 }
+
+TEST(Delaunator, issue_2)
+{
+    for (double angle = 0; angle < 360; angle += .1)
+        testRotation(angle);
+}
+
